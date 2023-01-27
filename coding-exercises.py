7a8b580e72794c2273db36f58bb1244d2544e799
pramodpaulk <pramodpaul2021@gmail.com>
2023-01-27 14:12:28 +0000

    Added Exercise-3 and launch.json for debugging?

--- coding-exercises.py
+++ coding-exercises.py
@@ -6,3 +6,6 @@ print("print('what to print')")
 # Exercise-2 String Manipulation
 print("Day 1 - String Manipulation \n" + "String Concatenation is done with the "+" sign. \n"
     + 'e.g. print("Hello" + "world")\n' + "New lines can be created with a backslash and n.")
+
+#Exercise-3 Using the len() and input() functions
+print(len(input("What is your name?")))
