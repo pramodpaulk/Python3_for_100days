c40677113ce42a15839e4eefb915ea777da8cedd
pramodpaulk <pramodpaul2021@gmail.com>
2023-01-28 19:50:14 +0000

    Corrected the percentage formula

--- Tip_calculator.py
+++ Tip_calculator.py
@@ -3,6 +3,6 @@ print("***Welcome to the tip calculator***")
 total_bill = eval(input("What was the total bill\n$"))
 total_people = eval(input("How many people to split the bill?\n"))
 tip_percentage = eval(input("What percentage tip would you like to give? 10, 12, or 15?\n"))
-total_incl_tip = total_bill + ((tip_percentage / total_bill) * 100)
+total_incl_tip = total_bill + (tip_percentage / 100 * total_bill)
 indiviual_split = total_incl_tip / 5
 print("Each person should pay:$", round(indiviual_split, 2))
