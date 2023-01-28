ffd45b5427d8f7c4f108c6fec2eff49eeae211d4
pramodpaulk <pramodpaul2021@gmail.com>
2023-01-28 21:32:50 +0000

    Day 2 exercises

---
+++ coding-exercises-day2.py
@@ -0,0 +1,5 @@
+'''Datatypes and string manipulation'''
+
+#Type conversion and adding two individual digits
+two_digit_number = input("Type a two digit number: ")
+print(int(str(two_digit_number[0])) + int(str(two_digit_number[1])))
