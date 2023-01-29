313baf375a08ceac08affa54bcdb77793c23ebd8
pramodpaulk <pramodpaul2021@gmail.com>
2023-01-29 17:37:27 +0000

    Added f string

--- Tip_calculator.py
+++ Tip_calculator.py
@@ -5,4 +5,4 @@ total_people = eval(input("How many people to split the bill?\n"))
 tip_percentage = eval(input("What percentage tip would you like to give? 10, 12, or 15?\n"))
 total_incl_tip = total_bill + (tip_percentage / 100 * total_bill)
 indiviual_split = total_incl_tip / 5
-print("Each person should pay:$", round(indiviual_split, 2))
+print(f"Each person should pay:${round(indiviual_split, 2)}")
