248de26b2554577e5be7d8f2cadbd7184a64fa3a
pramodpaulk <pramodpaul2021@gmail.com>
2023-01-29 17:27:13 +0000

    modified code for better reading

--- coding-exercises-day2.py
+++ coding-exercises-day2.py
@@ -10,7 +10,8 @@ years_left = 90 - int(age)
 months_left = years_left * 12
 weeks_left = years_left * 52
 days_left = years_left * 365
-print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left.")
+message = f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left."
+print(message)
 
 
 
