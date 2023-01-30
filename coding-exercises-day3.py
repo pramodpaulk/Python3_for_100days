3260857d0430cce9fb95c9cfab8f0a5f5e3a83cc
pramodpaulk <pramodpaul2021@gmail.com>
2023-01-30 22:32:19 +0000

    Added leap year exercise

--- coding-exercises-day3.py
+++ coding-exercises-day3.py
@@ -26,3 +26,13 @@ elif bmi < 35:
 else:
     print(f"Your BMI is {bmi}, you are clinically obese.")
 
+#Leap Year
+year = int(input("Which year do you want to check? "))
+
+if year % 4 == 0:
+   if year % 100 !=0 or year % 400 == 0:
+       print("Leap year.")
+   else:
+       print("Not leap year.")
+else:
+    print("Not leap year.")
