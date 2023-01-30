6ccffba774069b27b7c94145cc84abaeae48dc48
pramodpaulk <pramodpaul2021@gmail.com>
2023-01-30 00:36:53 +0000

    added additional features in life in weeks exercis

--- coding-exercises-day2.py
+++ coding-exercises-day2.py
@@ -10,8 +10,14 @@ years_left = 90 - int(age)
 months_left = years_left * 12
 weeks_left = years_left * 52
 days_left = years_left * 365
+hours_left = days_left * 24
+minutes_left = hours_left * 60
+seconds_left = minutes_left * 60
 message = f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left."
+message1 = f"You have {hours_left} hours, {minutes_left} minutes, and {seconds_left} seconds left."
 print(message)
+print(message1)
+
 
 #BMI Calculator
 height = input("enter your height in m: ")
