0831756bf9bd62cb2e0eeecc5cc6d47cc4f4ffb7
pramodpaulk <pramodpaul2021@gmail.com>
2023-02-01 22:00:11 +0000

    made the code readable

--- coding-exercises-day3.py
+++ coding-exercises-day3.py
@@ -70,9 +70,9 @@ first_digit = str(char_count('TRUE'))
 second_digit = str(char_count('LOVE'))
 score = int(first_digit + second_digit)
 
-if score < 10 or score > 90:
+if (score < 10) or (score > 90):
     print(f"Your score is {score}, you go together like coke and mentos.")
-elif score > 40 and score < 50:
+elif (score > 40) and (score < 50):
     print(f"Your score is {score}, you are alright together.")
 else:
     print(f"Your score is {score}.")
