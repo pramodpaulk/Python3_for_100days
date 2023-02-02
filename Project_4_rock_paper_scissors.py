a31840f5d6b77d3788ffa4ce2820c02c34659b0d
pramodpaulk <pramodpaul2021@gmail.com>
2023-02-02 23:33:11 +0000

    removed bugs

--- Project_4_rock_paper_scissors.py
+++ Project_4_rock_paper_scissors.py
@@ -28,19 +28,22 @@ scissors = '''
 
 #Write your code below this line 👇
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-value_map = {0: rock, 1: paper, 2: scissors}
+game_images = [rock, paper, scissors]
 computer_choice = random.randint(0, 2)
-print(value_map[user_choice])
-print(f"Computer chose \n{value_map[computer_choice]}")
-if user_choice == 0 and computer_choice == 2:
-    print("You win")
-elif user_choice == 2 and computer_choice == 1:
-    print("You win")
-elif user_choice == 2 and computer_choice == 0:
-    print("You lose")
-elif user_choice == 1 and computer_choice == 0:
-    print("You win")
-elif user_choice == computer_choice:
-    print("It's a Draw")
+
+if user_choice >= 3 or user_choice < 0:
+    print("You typed an invalid number, You lose")
 else:
-    print("You lose")
+    print(game_images[user_choice])
+    print(f"Computer chose \n{game_images[computer_choice]}")
+    if user_choice == 0 and computer_choice == 2:
+        print("You win")
+    elif user_choice == 2 and computer_choice == 1:
+        print("You win")
+    elif user_choice == 2 and computer_choice == 0:
+        print("You lose")
+    elif user_choice == 1 and computer_choice == 0:
+        print("You win")
+    elif user_choice == computer_choice:
+        print("It's a Draw")
+
