cef6cf99655e282449d05e759880f667d86a8e0c
pramodpaulk <pramodpaul2021@gmail.com>
2023-02-02 18:52:22 +0000

    Added banker roulette exercise

--- coding-exercises-day4.py
+++ coding-exercises-day4.py
@@ -1,7 +1,14 @@
-#Virtual coin toss
+# Virtual coin toss
 import random
-result = random.randint(0,1)
+result = random.randint(0, 1)
 if result == 0:
-     print("Tails")
+    print("Tails")
 else:
     print("Heads")
+
+# Banker Roulette
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+random_index = random.randint(0, len(names))
+print(f"{names[random_index]} is going to buy the meal today!")
