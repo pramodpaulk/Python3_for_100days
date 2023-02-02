0a40b4a67cb95ab223d5947134f8b968b9966e0e
pramodpaulk <pramodpaul2021@gmail.com>
2023-02-02 19:06:03 +0000

    Modified banker roulette exercise

--- coding-exercises-day4.py
+++ coding-exercises-day4.py
@@ -10,5 +10,5 @@ else:
 # Split string method
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
-random_index = random.randint(0, len(names))
+random_index = random.randint(0, len(names)-1)
 print(f"{names[random_index]} is going to buy the meal today!")
