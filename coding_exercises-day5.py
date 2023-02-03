4c976320ca86c68b3cd8ca08b6cb74b247a74d26
pramodpaulk <pramodpaul2021@gmail.com>
2023-02-03 16:56:53 +0000

    optimized code

--- coding_exercises-day5.py
+++ coding_exercises-day5.py
@@ -23,7 +23,7 @@ print(f"The highest score in the class is: {highest_score}")
 
 # Sum of even numbers in 1-100
 total = 0
-for number in range(1, 101):
-    if number % 2 == 0:
-        total += number
+for number in range(2, 101, 2):
+    total += number
 print(total)
+
