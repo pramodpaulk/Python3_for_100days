9a7a946ca478ed98bc9ddc0c6ca3bd4e57d12c31
pramodpaulk <pramodpaul2021@gmail.com>
2023-02-03 16:35:06 +0000

    Added highest score exercise

--- coding_exercises-day5.py
+++ coding_exercises-day5.py
@@ -9,3 +9,15 @@ for height in student_heights:
     total_count += 1
 average_height = total_height / total_count
 print(round(average_height))
+
+# highest score
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+print(f"The highest score in the class is: {highest_score}")
