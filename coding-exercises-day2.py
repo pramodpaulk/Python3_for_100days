67b3fa99d1487c77f84b304c0561387886473c27
pramodpaulk <pramodpaul2021@gmail.com>
2023-02-05 20:35:10 +0000

    reformatted code

--- coding-exercises-day2.py
+++ coding-exercises-day2.py
@@ -1,10 +1,10 @@
-'''Datatypes and string manipulation'''
+"""Datatypes and string manipulation"""
 
-#Type conversion and adding two individual digits
+# Type conversion and adding two individual digits
 two_digit_number = input("Type a two digit number: ")
 print(int(two_digit_number[0]) + int(two_digit_number[1]))
 
-#Life in Weeks
+# Life in Weeks
 age = input("What is your current age? ")
 years_left = 90 - int(age)
 months_left = years_left * 12
@@ -19,9 +19,9 @@ print(message)
 print(message1)
 
 
-#BMI Calculator
+# BMI Calculator
 def bmi_calculator():
     height = input("enter your height in m: ")
     weight = input("enter your weight in kg: ")
-    bmi = float(weight) / pow(float(height),2)
+    bmi = float(weight) / pow(float(height), 2)
     print(int(bmi))
