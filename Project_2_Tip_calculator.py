fb179239d7c71083dfa00d5e1afbe48694605315
pramodpaulk <pramodpaul2021@gmail.com>
2023-02-05 20:37:58 +0000

    reformatted code

--- Project_2_Tip_calculator.py
+++ Project_2_Tip_calculator.py
@@ -1,8 +1,8 @@
-#Tip Calculator
+# Tip Calculator
 print("***Welcome to the tip calculator***")
 total_bill = eval(input("What was the total bill\n$"))
 total_people = eval(input("How many people to split the bill?\n"))
 tip_percentage = eval(input("What percentage tip would you like to give? 10, 12, or 15?\n"))
-total_incl_tip = total_bill * (1 + tip_percentage/100)
-indiviual_split = "{:.2f}".format(total_incl_tip / 5)
-print(f"Each person should pay:${indiviual_split}")
+total_incl_tip = total_bill * (1 + tip_percentage / 100)
+individual_split = "{:.2f}".format(total_incl_tip / 5)
+print(f"Each person should pay:${individual_split}")
