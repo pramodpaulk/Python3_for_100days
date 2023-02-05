e068db1255b72857558ec1a321e1ce137ad62b51
pramodpaulk <pramodpaul2021@gmail.com>
2023-02-05 20:35:48 +0000

    reformatted code

--- coding-exercises-day1.py
+++ coding-exercises-day1.py
@@ -4,15 +4,15 @@ print("The function is declared like this:")
 print("print('what to print')")
 
 # Exercise-2 String Manipulation
-print("Day 1 - String Manipulation \n" + "String Concatenation is done with the "+" sign. \n"
-    + 'e.g. print("Hello" + "world")\n' + "New lines can be created with a backslash and n.")
+print("Day 1 - String Manipulation \n" + "String Concatenation is done with the " + " sign. \n"
+      + 'e.g. print("Hello" + "world")\n' + "New lines can be created with a backslash and n.")
 
-#Exercise-3 Using the len() and input() functions
+# Exercise-3 Using the len() and input() functions
 print(len(input("What is your name?")))
 
-#Exercise-4 Swapping two variables
+# Exercise-4 Swapping two variables
 a = input("a: ")
 b = input("b: ")
-a,b = b,a #Tuple to swap the variables
+a, b = b, a  # Tuple to swap the variables
 print("a: " + a)
 print("b: " + b)
