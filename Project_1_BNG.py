ee4281d4f4ed6a6e7fcff6949ac86490eefa1211
pramodpaulk <pramodpaul2021@gmail.com>
2023-02-05 20:37:03 +0000

    reformatted code

--- Project_1_BNG.py
+++ Project_1_BNG.py
@@ -1,10 +1,12 @@
-''' Project-1 Band name idea generator '''
+""" Project-1 Band name idea generator """
+
+
 # Function to generate the band name
 def band_name_gen():
     Q1 = input("What is your first name? \n")
     Q2 = input("What is your pet name? \n")
-    return Q1 + ' ' +  Q2
+    return Q1 + ' ' + Q2
+
 
 # To Print the final statement and execute the function
-print('Your Band Name can be.... \n',band_name_gen())
-
+print('Your Band Name can be.... \n', band_name_gen())
