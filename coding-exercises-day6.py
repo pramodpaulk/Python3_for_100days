5df2f34f379ba0f259abff6e1dabae34c7574b64
pramodpaulk <pramodpaul2021@gmail.com>
2023-02-06 23:20:04 +0000

    Added hurdles race with variable heights exercise

--- coding-exercises-day6.py
+++ coding-exercises-day6.py
@@ -48,3 +48,39 @@ while not at_goal():
         jump()
     else:
         move()
+
+# Jumping hurdles with variable heights
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    if wall_on_right():
+        turn_left()
+    elif front_is_clear():
+        move()
+    else:
+        turn_right()
+        move()
+        turn_right()
+        move()
+
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    elif wall_on_right():
+        move()
+    else:
+        turn_right()
+        move()
+        turn_right()
+        move()
+
+
+
+
+
