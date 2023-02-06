ecf200152210134eded8969bcfed3838e1459bdd
pramodpaulk <pramodpaul2021@gmail.com>
2023-02-06 22:23:12 +0000

    Added hurdles race exercise

--- coding-exercises-day6.py
+++ coding-exercises-day6.py
@@ -20,7 +20,31 @@ def jump():
 # for or while can be used
 for step in range(6):
     jump()
-
 # Using while loop
 while not at_goal():
     jump()
+
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+# Hurdles race
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
